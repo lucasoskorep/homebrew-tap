36a095d52998c6cbbb93f8bb2894af6239fb22d3
Lucas Oskorep <lucas.oskorep@rokt.com>
2026-04-13 15:31:34 -0400

    fix: declare aerospace manpages individually

--- Casks/aerospace-ghostty-tabs.rb
+++ Casks/aerospace-ghostty-tabs.rb
@@ -16,7 +16,9 @@ cask "aerospace-ghostty-tabs" do
   bash_completion "#{package_root}/shell-completion/bash/aerospace"
   zsh_completion "#{package_root}/shell-completion/zsh/_aerospace"
   fish_completion "#{package_root}/shell-completion/fish/aerospace.fish"
-  manpage Dir["#{staged_path}/#{package_root}/manpage/*.1"]
+  Dir["#{package_root}/manpage/*.1"].sort.each do |page|
+    manpage page
+  end
 
   postflight do
     system "/usr/bin/xattr", "-dr", "com.apple.quarantine", "#{staged_path}/#{package_root}/bin/aerospace"
